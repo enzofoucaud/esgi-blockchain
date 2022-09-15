5ff290a4417db7d424539a89392c436aff540f9b
Enzo Foucaud <foucauden@gmail.com>
2022-09-15 10:42:31 +0200

    WIP: First test deposit withdraw

--- contracts/cooldown.sol
+++ contracts/cooldown.sol
@@ -2,6 +2,64 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract cooldown {
-  constructor() public {
-  }
+    // Enum status of order
+    enum OrderStatus {
+        Created,
+        Completed
+    }
+
+    /// The Order struct
+    struct Order {
+        uint256 id;
+        address sender;
+        address receiver;
+        uint256 amount;
+        uint256 deadline;
+        OrderStatus status;
+    }
+
+    /// The mapping to store orders
+    mapping(uint256 => Order) orders;
+
+    /// The sequence number of orders
+    uint256 orderseq;
+
+    event Deposit(address sender, uint256 amount);
+    event Withdraw(address sender);
+
+    function deposit(uint deadline) public payable {
+        /// Increment the order sequence
+        orderseq++;
+
+        /// Create the order register
+        orders[orderseq] = Order(
+            orderseq,
+            msg.sender,
+            address(0),
+            msg.value,
+            deadline,
+            OrderStatus.Created
+        );
+
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function withdraw(uint256 id) public {
+        /// Get the order
+        Order storage order = orders[id];
+
+        /// Check the order status
+        require(order.status == OrderStatus.Created, "Order is not created");
+
+        /// Check the deadline
+        require(order.deadline > block.timestamp, "Order is not completed");
+
+        /// Transfer the amount to sender
+        payable(order.sender).transfer(order.amount);
+
+        /// Update the order status
+        order.status = OrderStatus.Completed;
+
+        emit Withdraw(msg.sender);
+    }
 }
